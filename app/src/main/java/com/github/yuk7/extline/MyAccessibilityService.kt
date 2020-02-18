e2eae9edfd2de1d05d82cff5246c6f8f5b87af7c
yuk7 <yukx00@gmail.com>
2020-02-19 02:31:37 +0900

    add AccessibilityService

---
+++ app/src/main/java/com/github/yuk7/extline/MyAccessibilityService.kt
@@ -0,0 +1,15 @@
+package com.github.yuk7.extline
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+    override fun onInterrupt() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
