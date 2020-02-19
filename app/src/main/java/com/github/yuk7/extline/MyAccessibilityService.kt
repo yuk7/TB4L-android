6079572e512e3209cc1a0e0d369fa8fe2c42bb09
yuk7 <yukx00@gmail.com>
2020-02-19 09:05:00 +0900

    MyAccessibilityService: fix nullpointerexception

--- app/src/main/java/com/github/yuk7/extline/MyAccessibilityService.kt
+++ app/src/main/java/com/github/yuk7/extline/MyAccessibilityService.kt
@@ -20,19 +20,20 @@ class MyAccessibilityService : AccessibilityService() {
         if (iab_header_url_nodes.size > 0) {
             val parent = iab_header_url_nodes[0] as AccessibilityNodeInfo
             try {
-                val url_text = parent.text.toString()
+                val url_text = (parent.text?:"").toString()
 
-                val iab_header_close_nodes = source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_close")
-                if (iab_header_close_nodes.size > 0) {
-                    val parent =iab_header_close_nodes[0] as AccessibilityNodeInfo
-                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                if(url_text.contains("http://") || url_text.contains("https://")) {
+                    val iab_header_close_nodes = source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_close")
+                    if (iab_header_close_nodes.size > 0) {
+                        val parent =iab_header_close_nodes[0] as AccessibilityNodeInfo
+                        parent.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                    }
+
+                    Thread.sleep(200)
+                    val i = Intent(Intent.ACTION_VIEW, Uri.parse(url_text))
+                    i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    applicationContext.startActivity(i)
                 }
-
-                Thread.sleep(200)
-                val i = Intent(Intent.ACTION_VIEW, Uri.parse(url_text))
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                applicationContext.startActivity(i)
-
             } catch (e : Exception) {
                 Log.e("ERR",Log.getStackTraceString(e))
             }
