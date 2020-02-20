4287713220d75ab1e34e55fe8351da3457ab4a10
yuk7 <yukx00@gmail.com>
2020-02-20 23:10:28 +0900

    MyAccessibilityService: Refactor

--- app/src/main/java/com/github/yuk7/extline/MyAccessibilityService.kt
+++ app/src/main/java/com/github/yuk7/extline/MyAccessibilityService.kt
@@ -16,33 +16,27 @@ class MyAccessibilityService : AccessibilityService() {
     var lastWindowId = 0
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         val source = event!!.source ?: return
-        val ihUrlNodes =
-            source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_url")
-        if (ihUrlNodes.size > 0) {
-            val parent = ihUrlNodes[0]
-            try {
-                val urlText = (parent.text ?: "").toString()
+
+        val ihCloseNodes =
+            source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_close")
+
+        if (ihCloseNodes.size > 0) {
+            if (ihCloseNodes[0].windowId == lastWindowId) {
+                ihCloseNodes[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                return
+            }
+            val ihUrlNodes =
+                source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_url")
+            if (ihUrlNodes.size > 0) {
+                val urlText = (ihUrlNodes[0].text ?: "").toString()
 
                 if (urlText.contains("http://") || urlText.contains("https://")) {
-                    if (parent.windowId != lastWindowId) {
-                        val i = Intent(Intent.ACTION_VIEW, Uri.parse(urlText))
-                        i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        applicationContext.startActivity(i)
-                        lastWindowId = parent.windowId
-                    } else {
-                        val ihCloseNodes =
-                            source.findAccessibilityNodeInfosByViewId("jp.naver.line.android:id/iab_header_close")
-                        if (ihCloseNodes.size > 0) {
-                            ihCloseNodes[0].performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        }
-                    }
+                    val i = Intent(Intent.ACTION_VIEW, Uri.parse(urlText))
+                    i.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    applicationContext.startActivity(i)
+                    lastWindowId = ihCloseNodes[0].windowId
                 }
-            } catch (e: Exception) {
-                Log.e("ERR", Log.getStackTraceString(e))
             }
         }
-
-
     }
-
 }
