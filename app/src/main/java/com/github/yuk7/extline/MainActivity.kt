af02b8526f2c74ca332ac31fe1b1582bfab03e17
yuk7 <yukx00@gmail.com>
2020-02-20 23:47:22 +0900

    MainActivity: add simple instructions

--- app/src/main/java/com/github/yuk7/extline/MainActivity.kt
+++ app/src/main/java/com/github/yuk7/extline/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +18,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(!isAccessibilitySettingsOn(this)) {
+        findViewById<TextView>(R.id.textView).text =
+            getString(R.string.accessibility_disabled_message)
+        findViewById<Button>(R.id.btnOpenSettings).text =
+            getString(R.string.accessibility_open_settings)
+
+        findViewById<Button>(R.id.btnOpenSettings).setOnClickListener {
             startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
         }
-
     }
 
-    private fun isAccessibilitySettingsOn(context: Context) : Boolean {
-        val res = Settings.Secure.getInt(context.applicationContext.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
-        if(res != 1) return false
+    override fun onResume() {
+        super.onResume()
+        if (isAccessibilitySettingsOn(this)) {
+            findViewById<TextView>(R.id.textView).text =
+                getString(R.string.accessibility_enabled_message)
+            findViewById<Button>(R.id.btnOpenSettings).isVisible = false
+        }
+    }
+
+    private fun isAccessibilitySettingsOn(context: Context): Boolean {
+        val res = Settings.Secure.getInt(
+            context.applicationContext.contentResolver,
+            Settings.Secure.ACCESSIBILITY_ENABLED
+        )
+        if (res != 1) return false
 
         val serviceName = packageName + "/" + MyAccessibilityService::class.java.canonicalName
-        val settingValue = Settings.Secure.getString(context.applicationContext.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+        val settingValue = Settings.Secure.getString(
+            context.applicationContext.contentResolver,
+            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
+        )
 
-        for(pkg in settingValue.split(":")) {
-            if(pkg.contains(serviceName)) {
+        for (pkg in settingValue.split(":")) {
+            if (pkg.contains(serviceName)) {
                 return true
             }
         }
